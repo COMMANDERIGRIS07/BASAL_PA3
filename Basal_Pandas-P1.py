c7e13e85de96df96360299bb450b06d40082702e
Earl David Basal <earldavid.basal.eng@ust.edu.ph>
2025-09-16 23:51:48 +0800

    Add files via upload

---
+++ Basal_Pandas-P1.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # PROBLEM 1
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[4]:
+
+
+cars = pd.read_csv('cars.csv')
+cars
+
+
+# In[5]:
+
+
+cars.head()
+
+
+# In[6]:
+
+
+cars.tail()
+
